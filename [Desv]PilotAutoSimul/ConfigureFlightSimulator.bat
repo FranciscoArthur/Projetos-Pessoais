a02123805f8e683fce3d800c25593db9c287595e
Arthur's notebook <arthurazevedo41@gmail.com>
2019-10-22 21:12:19 +0200

    Create ConfigureFlightSimulator.bat

---
+++ [Desv]PilotAutoSimul/ConfigureFlightSimulator.bat
@@ -0,0 +1,7 @@
+C:
+
+cd C:\Program Files\FlightGear 2018.3.4\
+
+SET FG_ROOT=C:\Program Files\FlightGear 2018.3.4\\data
+
+.\\bin\fgfs --aircraft=Mirage_F1 --fdm=null --native-fdm=socket,in,30,127.0.0.1,5502,udp --native-ctrls=socket,out,30,127.0.0.1,5505,udp --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --enable-freeze --airport=KSFO --runway=10L --altitude=7224 --heading=113 --offset-distance=4.72 --offset-azimuth=0 --enable-terrasync --prop:/sim/rendering/shaders/quality-level=0
